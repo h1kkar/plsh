cf5d5bcb2d019d5c47d6c6e62e4c0f1c4e731717
h1kkar <dmitrijsinicin15@gmail.com>
2023-07-30 16:31:47 +0700

    added work with git branch and commits

--- src/shime/prompt/git.rs
+++ src/shime/prompt/git.rs
@@ -1,16 +1,67 @@
-
 pub struct Conf {
     pub branch: String,
-    pub username: String,
     pub noncommited: bool,
     pub lastcommit: String,
+}
+
+pub mod get {
+    use std::{
+        process::Command,
+        str::from_utf8,
+    };
+    use ansi_colors::ColouredStr;
+
+    pub fn branch() -> String {
+        let branch = super::config().branch;
+        let mut b = ColouredStr::new(&branch);
+        b.magenta();
+        b.bold();
+        let s = String::from(" on ") + &b.to_string();
+        return s
+    }
+    
+    pub fn dirty() -> String {
+        let mut c = ColouredStr::new("* ");
+        c.magenta();
+        c.bold();
+        if super::config().noncommited {
+            return c.to_string()
+        } else {
+            String::from("")
+        }
+    }
+    
+    pub fn commit() -> String {
+        let commit = super::config().lastcommit;
+        let mut c = ColouredStr::new(&commit);
+        c.light_blue();
+        c.bold();
+        let s = c.to_string() + " ";
+        return s
+    }
+    
+    pub fn status() -> bool {
+        let g = match Command::new("git")
+            .arg("branch")
+            .output() {
+                Ok(p) => {
+                    Some(from_utf8(p.stdout.as_slice()).unwrap().to_string())
+                },
+                Err(_) => None
+            };
+        
+        if g != None {
+            return true
+        } else {
+            return false
+        }
+    }
 }
 
 pub fn config() -> Conf {
     let info = git_info::get();
     Conf {
         branch: info.current_branch.unwrap_or(alt_conf::branch()),
-        username: info.user_name.unwrap_or("".to_string()),
         noncommited: info.dirty.unwrap_or(false),
         lastcommit: info.head.last_commit_hash_short.unwrap_or("".to_string())
     }
