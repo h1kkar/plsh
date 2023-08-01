7b728b8f9a8f2275e20036a0283413206d5ea5ff
h1kkar <dmitrijsinicin15@gmail.com>
2023-08-01 12:49:03 +0700

    redesign shime::prompt::get module

--- src/shime/prompt.rs
+++ src/shime/prompt.rs
@@ -46,49 +46,7 @@ fn line() {
     print!("\n")
 }
 
-pub mod get {
-    use super::*;
-    
-    pub fn root() {
-        print!("{0}", sudo::root())
-    }
-    
-    pub fn dir() {
-        print!("{0}", dir::main())
-    }
-    
-    pub fn branch() {
-        print!("{0}{1}", git::get::branch(), git::get::dirty())
-    }
-    
-    pub fn commit() {
-        print!("{0}", git::get::commit())
-    }
-    
-    pub fn delim() {
-        print!("❘ ")
-    }
-    
-    pub fn pkgver() {
-        print!("{0}",super::cargo::pkg_ver())
-    }
-    
-    pub fn rustver() {
-        print!("{}", super::cargo::rust_ver())
-    }
-    
-    pub fn history() -> String {
-        match dirs::config_dir() {
-        Some(path) => {
-            let mut path = path.display().to_string();
-            path.push_str("/shime/shime_history");
-            path
-        },
-        None => "".to_string()
-    }
-    }
-}
-
+pub mod get;
 pub mod dye;
 pub mod dir;
 pub mod git;
